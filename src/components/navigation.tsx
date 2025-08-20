ff7fe020a6a1e89214f0ff77982e2b7b095541d9
Priscilla Ong <141508555+PriscillaOng12@users.noreply.github.com>
2025-08-20 13:55:41 +0800

    Update navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -9,7 +9,6 @@ const navigation = [
   { name: 'About', href: '#about' },
   { name: 'Projects', href: '#projects' },
   { name: 'Skills', href: '#skills' },
-  { name: 'Contact', href: '#contact' },
 ];
 
 export function Navigation() {
